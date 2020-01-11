1ed5cc1d8739f803e05146b97a35da7c406de8a3
Gavin Williams <gamichwi@gmail.com>
2020-01-11 15:15:15 +1100

    useEffect to get EPA data

--- src/Components/AirQuality.js
+++ src/Components/AirQuality.js
@@ -1,8 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const getEPAEnvMonitoring = async () => {
+    const response = await axios.get('https://www.epa.vic.gov.au/api/envmonitoring/sites?environmentalSegment=air');
+    console.log(response, 'response');
+    return response
+}
 
 const AirQuality = props => {
+    const [counter, setCounter] = useState(0);
+    const [header, setHeader] = useState('');
+    const [sites, setSites] = useState([]);
+
+    useEffect(() => {
+        const EPA = getEPAEnvMonitoring()
+        EPA.then((value) => {
+            if (value.data) {
+                setSites(value.data.records);
+            }
+        })
+    }, [])
+    
+    useEffect(() => {
+        console.log('useEffect in AirQuality')
+    },[])
+
+    const increment = () => {
+        setCounter(counter + 1);
+    }
+
+    const handleChange = event => {
+        setHeader(event.target.value);
+    }
+
     return (
-        <h1>Air Quality:</h1>
+        <div className="air-quality-component">
+        <h1>EPA - Air Quality</h1>
+        <h2>{header}</h2>
+        <h2>{counter}</h2>
+        <button onClick={increment}>Increment</button>
+        <input onChange={handleChange} />
+        </div>
     )
 }
+
 export default AirQuality;
