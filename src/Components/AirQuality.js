52a5d37a2126cc351f04c480e8d1fe76804b13de
Gavin Williams <gamichwi@gmail.com>
2020-01-11 17:04:00 +1100

    Added cards for site details

--- src/Components/AirQuality.js
+++ src/Components/AirQuality.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { ListGroup, Container, Col, Row } from "react-bootstrap";
+import { ListGroup, Container, Col, Row, Card } from "react-bootstrap";
 
-import './AirQuality.css'
+import "./AirQuality.css";
 
 const getEPAEnvMonitoring = async () => {
   const response = await axios.get(
@@ -14,6 +14,7 @@ const getEPAEnvMonitoring = async () => {
 
 const AirQuality = props => {
   const [sites, setSites] = useState([]);
+  const [siteDetails, setSiteDetails] = useState(null);
 
   useEffect(() => {
     const EPA = getEPAEnvMonitoring();
@@ -28,27 +29,52 @@ const AirQuality = props => {
     console.log("useEffect in AirQuality");
   }, []);
 
+  const onSiteClick = listIndex => {
+    console.log(listIndex, "listindex");
+    const siteRecord = sites[listIndex];
+    console.log(siteRecord);
+    setSiteDetails(siteRecord);
+  };
+
   return (
     <div className="air-quality-component">
       <h1>EPA - Air Quality</h1>
       <Container>
-          <Row>
-              <Col>
-      <ListGroup>
-        {sites.map(site => {
-            const style = {
-                backgroundColor: site.siteHealthAdvices ? site.siteHealthAdvices[0].healthAdviceColor : ''
-            }
-          return (
-            <ListGroup.Item key={site.siteID} className="site-list" style={style}>
-              {site.siteName}
-            </ListGroup.Item>
-          );
-        })}
-      </ListGroup>
-      </Col>
-      </Row>
-    </Container>
+        <Row>
+          <Col>
+            <ListGroup>
+              {sites.map((site, index) => {
+                const style = {
+                  backgroundColor: site.siteHealthAdvices
+                    ? site.siteHealthAdvices[0].healthAdviceColor
+                    : ""
+                };
+                return (
+                  <ListGroup.Item
+                    key={site.siteID}
+                    className="site-list"
+                    style={style}
+                    onClick={() => onSiteClick(index)}
+                  >
+                    {site.siteName}
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          </Col>
+          <Col>
+            <h3>Details</h3>
+            {siteDetails && <Card>
+              <Card.Body>
+                <Card.Title>{siteDetails.siteName}</Card.Title>
+                {siteDetails.siteHealthAdvices && <Card.Subtitle>Air quality: {siteDetails.siteHealthAdvices[0].healthAdvice} </Card.Subtitle>}
+                <Card.Img variant="top" src={siteDetails.cameraImageURL} />
+              </Card.Body>
+            </Card>
+        }
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
